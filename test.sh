63948dffab53a0a73d85aca7948474cd5a4603f7
Prajyot Thakur <prajyotthakkur@gmail.com>
2023-07-16 13:17:52 +0530

    test script added

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Prompt the user for their name
+echo "What is your name?"
+
+# Read the user's input
+read name
+
+# Greet the user
+echo "Hello, $name! Welcome to the world of shell scripting."
